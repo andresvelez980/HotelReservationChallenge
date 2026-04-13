c616c624edcefe08d4f564bd9f87e6f7b7057e76
Andres felipe <andrew.vm1611@gmail.com>
2026-04-13 16:40:17 -0500

    se agrega la clase Resrervation y los metodos

--- app/model/hotel.py
+++ app/model/hotel.py
@@ -22,7 +22,36 @@ class Guest:
         return f"Guest {self.name} ({self.email}) of type {self.type_}"
 
 
-# TODO: Implement Reservation class here
+@dataclass
+class Reservation:
+    guest_name: str
+    description: str
+    check_in: date
+    check_out: date
+
+    guests: list[Guest] = field(default_factory=list)
+    id: str = field(default_factory=generate_unique_id)
+
+    def add_guest(self, name: str, email: str, type_: str):
+        guest = Guest(name, email, type_)
+        self.guests.append(guest)
+
+    def delete_guest(self, guest_index: int):
+        if 0 <= guest_index < len(self.guests):
+            self.guests.pop(guest_index)
+        else:
+            guest_not_found_error()
+
+    def __len__(self) -> int:
+        return (self.check_out - self.check_in).days
+
+    def __str__(self) -> str:
+        return (
+            f"ID: {self.id}\n"
+            f"Guest: {self.guest_name}\n"
+            f"Description: {self.description}\n"
+            f"Dates: {self.check_in} - {self.check_out}"
+        )
 
 
 # TODO: Implement Room class here
